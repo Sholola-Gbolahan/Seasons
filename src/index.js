7482f305011d201a43a34cd3f1f4b6fd9b1241f7
Gbolahan Sholola <sholola.gbolahan@gurugeeksroyalty.biz>
2023-08-17 08:03:35 +0100

    packages install and project initiate

--- src/index.js
+++ src/index.js
@@ -1,17 +1,49 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+class App extends React.Component {
+  // Constructor is the first to get called
+  constructor(props) {
+    // super is called as reference to the parent constructor
+    super(props);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    this.state = { lat: null, long: null, errormessage: "" };
+
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        // we call setState obj to update our state
+        this.setState({ lat: position.coords.latitude });
+        this.setState({ long: position.coords.longitude });
+      },
+      (err) => {
+        this.setState({ errormessage: err.message });
+      }
+    );
+  }
+  componentDidMount(){
+    console.log('My component was rendered to the screen')
+  }
+  componentDidUpdate(){
+    console.log('My component was just updated - it rerendered')
+  }
+
+  // Render() method is needed to be define
+  render() {
+    if (this.state.errormessage && !this.state.lat && !this.state.long) {
+      return <div>{this.state.errormessage}</div>;
+    }
+
+    if (!this.state.errormessage && this.state.lat && this.state.long) {
+      return (
+        <div>
+          long: {this.state.long}<br />
+          lat:{this.state.lat}
+        </div>
+      );
+    }
+
+    return <div> Loading!</div>;
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector("#root"));
