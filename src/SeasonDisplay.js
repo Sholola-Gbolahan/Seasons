4873c731720a100e03714b49734ef5d28f008c41
Gbolahan Sholola <sholola.gbolahan@gurugeeksroyalty.biz>
2023-09-29 13:03:22 +0100

    Added styling to season App.

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,33 +1,46 @@
-import React from 'react';
+import React from "react";
+import { BsSnow2, BsFillSunFill } from "react-icons/bs";
+import './SeasonDisplay.css'
+
+const seasonConfig = {
+  summer: {
+    text: "It's Really Hot Here",
+      icon: <BsFillSunFill  size={100} />,
+  },
+  winter: {
+    text: "It's Really Cold Here",
+    icon: <BsSnow2 size={100} />,
+  },
+};
 
 const getSeason = (lat, month) => {
-  if (month > 2 && month < 9 ){
+
+  if (month > 2 && month < 9) {
     // --------------------------------------------------------------------------------------------------- //
-       // This is a javascript ternary expression
-   return  lat > 0  ? 'summer' : 'winter';
-       // This expression is saying if lat is > 0 is should return summer and if it's less than o is should return 'winter'
-      //  If we are in the summer month and we are in the Northan atmoshere return summer other wise retuen winter 
+    // This is a javascript ternary expression
+    return lat > 0 ? "summer" : "winter";
+    // This expression is saying if lat is > 0 is should return summer and if it's less than o is should return 'winter'
+    //  If we are in the summer month and we are in the Northan atmoshere return summer other wise return winter
+
     // ---------------------------------------------------------------------------------------------------- //
-  }
-  else {
-    return lat > 0 ? 'winter' : 'summer'
-  }
+  } else {
 
-}
+    return lat > 0 ? "winter" : "summer";
 
+  }
+  
+};
 
 const SeasonDisplay = (props) => {
-
-  const season = getSeason(props.lat, new Date().getMonth())
-  const text =  season === 'winter' ? "Burr, it's chilly" : "Let hit the beach";
-  const icon =   season === 'winter' ? "snowflake" : "sun";
+  const season = getSeason(props.lat, new Date().getMonth());
+  const { text, icon } = seasonConfig[season];
   return (
-    <div>
-      <i className={ `${icon} icon`}/>
-       <h1>{text}</h1> 
-       <i className={ `${icon} icon`}/>
+    <div className={`season-display ${season}`}>
+    <div className="icon-left">{icon}</div>
+      <h1>{text}</h1>
+    <div className="icon-right">{icon}</div>
     </div>
-  )
-}
+  );
+};
 
-export default SeasonDisplay 
+export default SeasonDisplay;
--- src/index.js
+++ src/index.js
@@ -2,53 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import SeasonDisplay from "./SeasonDisplay";
 
-// class App extends React.Component {
-//   // Constructor is the first to get called
-//   constructor(props) {
-//     // super is called as reference to the parent constructor
-//     super(props);
-
-//    THIS IS THE ONLY TIME WE DO DIRECT ASSIGNMENT
-//     TO THIS STATE
-//     this.state = { lat: null, long: null, errormessage: "" };
-
-//     window.navigator.geolocation.getCurrentPosition(
-//       (position) => {
-//         // we call setState obj to update our state
-//         this.setState({ lat: position.coords.latitude });
-//         this.setState({ long: position.coords.longitude });
-//       },
-//       (err) => {
-//         this.setState({ errormessage: err.message });
-//       }
-//     );
-//   }
-//   componentDidMount(){
-//     console.log('My component was rendered to the screen')
-//   }
-//   componentDidUpdate(){
-//     console.log('My component was just updated - it rerendered')
-//   }
-
-//   // Render() method is needed to be define
-//   render() {
-//     if (this.state.errormessage && !this.state.lat && !this.state.long) {
-//       return <div>{this.state.errormessage}</div>;
-//     }
-
-//     if (!this.state.errormessage && this.state.lat && this.state.long) {
-//       return (
-//         <div>
-//           long: {this.state.long}<br />
-//           lat:{this.state.lat}
-//         </div>
-//       );
-//     }
-
-//     return <div> Loading!</div>;
-//   }
-// }
-
+// class 
 class App extends React.Component {
 
   state = { lat: null, log: null, errorMessage: "" };
