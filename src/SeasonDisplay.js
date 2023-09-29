353424816fe7b96a87c27b01ef221fded92c93f8
Gbolahan Sholola <sholola.gbolahan@gurugeeksroyalty.biz>
2023-09-29 11:06:23 +0100

    Added text and icons to seaosn display

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from 'react';
 
-const SeasonDisplay = () => {
+const getSeason = (lat, month) => {
+  if (month > 2 && month < 9 ){
+    // --------------------------------------------------------------------------------------------------- //
+       // This is a javascript ternary expression
+   return  lat > 0  ? 'summer' : 'winter';
+       // This expression is saying if lat is > 0 is should return summer and if it's less than o is should return 'winter'
+      //  If we are in the summer month and we are in the Northan atmoshere return summer other wise retuen winter 
+    // ---------------------------------------------------------------------------------------------------- //
+  }
+  else {
+    return lat > 0 ? 'winter' : 'summer'
+  }
+
+}
+
+
+const SeasonDisplay = (props) => {
+
+  const season = getSeason(props.lat, new Date().getMonth())
+  const text =  season === 'winter' ? "Burr, it's chilly" : "Let hit the beach";
+  const icon =   season === 'winter' ? "snowflake" : "sun";
   return (
-    <div>SeasonDisplay</div>
+    <div>
+      <i className={ `${icon} icon`}/>
+       <h1>{text}</h1> 
+       <i className={ `${icon} icon`}/>
+    </div>
   )
 }
 
-export default SeasonDisplay
+export default SeasonDisplay 
--- src/index.js
+++ src/index.js
@@ -1,49 +1,83 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import SeasonDisplay from "./SeasonDisplay";
+
+// class App extends React.Component {
+//   // Constructor is the first to get called
+//   constructor(props) {
+//     // super is called as reference to the parent constructor
+//     super(props);
+
+//    THIS IS THE ONLY TIME WE DO DIRECT ASSIGNMENT
+//     TO THIS STATE
+//     this.state = { lat: null, long: null, errormessage: "" };
+
+//     window.navigator.geolocation.getCurrentPosition(
+//       (position) => {
+//         // we call setState obj to update our state
+//         this.setState({ lat: position.coords.latitude });
+//         this.setState({ long: position.coords.longitude });
+//       },
+//       (err) => {
+//         this.setState({ errormessage: err.message });
+//       }
+//     );
+//   }
+//   componentDidMount(){
+//     console.log('My component was rendered to the screen')
+//   }
+//   componentDidUpdate(){
+//     console.log('My component was just updated - it rerendered')
+//   }
+
+//   // Render() method is needed to be define
+//   render() {
+//     if (this.state.errormessage && !this.state.lat && !this.state.long) {
+//       return <div>{this.state.errormessage}</div>;
+//     }
+
+//     if (!this.state.errormessage && this.state.lat && this.state.long) {
+//       return (
+//         <div>
+//           long: {this.state.long}<br />
+//           lat:{this.state.lat}
+//         </div>
+//       );
+//     }
+
+//     return <div> Loading!</div>;
+//   }
+// }
 
 class App extends React.Component {
-  // Constructor is the first to get called
-  constructor(props) {
-    // super is called as reference to the parent constructor
-    super(props);
 
-    this.state = { lat: null, long: null, errormessage: "" };
+  state = { lat: null, log: null, errorMessage: "" };
 
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
       (position) => {
-        // we call setState obj to update our state
-        this.setState({ lat: position.coords.latitude });
-        this.setState({ long: position.coords.longitude });
+           this.setState({ lat: position.coords.latitude })
+        this.setState({ log: position.coords.longitude })
       },
-      (err) => {
-        this.setState({ errormessage: err.message });
-      }
+     
+      (err) => this.setState({errorMessage:err.message})
     );
   }
-  componentDidMount(){
-    console.log('My component was rendered to the screen')
-  }
-  componentDidUpdate(){
-    console.log('My component was just updated - it rerendered')
-  }
 
-  // Render() method is needed to be define
   render() {
-    if (this.state.errormessage && !this.state.lat && !this.state.long) {
-      return <div>{this.state.errormessage}</div>;
+    
+    if(this.state.errorMessage){
+      return <div><p>Error:{this.state.errorMessage}</p></div>;
     }
 
-    if (!this.state.errormessage && this.state.lat && this.state.long) {
-      return (
-        <div>
-          long: {this.state.long}<br />
-          lat:{this.state.lat}
-        </div>
-      );
+    if(!this.state.errorMessage && this.state.log) {
+      return  <div> <SeasonDisplay lat= {this.state.lat} log = {this.state.log} /> </div>
     }
+  
+            return <div> <p>Loading......</p></div>;
 
-    return <div> Loading!</div>;
-  }
+    
+  } 
 }
 
 ReactDOM.render(<App />, document.querySelector("#root"));
